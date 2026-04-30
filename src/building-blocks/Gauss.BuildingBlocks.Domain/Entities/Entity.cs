eaab45c86285cd8e00825efa504e5e7636362805
Jeferson Almeida <jeffreys.sharp@outlook.com>
2026-04-30 08:17:13 -0300

    feat(identity): add secure user domain model

---
+++ src/building-blocks/Gauss.BuildingBlocks.Domain/Entities/Entity.cs
@@ -0,0 +1,37 @@
+namespace Gauss.BuildingBlocks.Domain.Entities;
+
+public abstract class Entity<TId>
+    where TId : notnull
+{
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public TId Id { get; protected init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity<TId> other)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        if (GetType() != other.GetType())
+        {
+            return false;
+        }
+
+        return EqualityComparer<TId>.Default.Equals(Id, other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(GetType(), Id);
+    }
+}
