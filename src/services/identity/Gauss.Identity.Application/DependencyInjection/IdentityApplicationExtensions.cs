20e6c026a7314e9b8efaa33712b92038bcf552c4
Jeferson Almeida <jeffreys.sharp@outlook.com>
2026-05-08 20:52:01 -0300

    refactor(identity): consolidate Identity Application registration and clean API ApplicationInstaller

---
+++ src/services/identity/Gauss.Identity.Application/DependencyInjection/IdentityApplicationExtensions.cs
@@ -0,0 +1,33 @@
+using FluentValidation;
+using Gauss.BuildingBlocks.Application.Abstractions.Messaging;
+using Gauss.Identity.Application.Abstractions.Authorization;
+using Gauss.Identity.Application.Authentication.Login;
+using Gauss.Identity.Application.Authentication.RefreshTokens;
+using Gauss.Identity.Application.Authorization;
+using Gauss.Identity.Application.Authorization.GetPermissions;
+using Gauss.Identity.Application.Users.RegisterUser;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gauss.Identity.Application.DependencyInjection;
+
+public static class IdentityApplicationExtensions
+{
+    public static IServiceCollection AddIdentityApplication(this IServiceCollection services)
+    {
+        services.AddCommandHandlerWithValidation<RegisterUserCommand, RegisterUserResponse, RegisterUserCommandHandler>();
+        services.AddCommandHandlerWithValidation<LoginCommand, LoginResponse, LoginCommandHandler>();
+        services.AddCommandHandlerWithValidation<RefreshTokenCommand, RefreshTokenResponse, RefreshTokenCommandHandler>();
+
+        services.AddScoped<IValidator<RegisterUserCommand>, RegisterUserCommandValidator>();
+        services.AddScoped<IValidator<LoginCommand>, LoginCommandValidator>();
+        services.AddScoped<IValidator<RefreshTokenCommand>, RefreshTokenCommandValidator>();
+
+        services.AddScoped<IPermissionAuthorizationService, PermissionAuthorizationService>();
+
+        services.AddScoped<GetPermissionsQueryHandler>();
+        services.AddScoped<IQueryHandler<GetPermissionsQuery, IReadOnlyCollection<GetPermissionResponse>>>(
+            sp => sp.GetRequiredService<GetPermissionsQueryHandler>());
+
+        return services;
+    }
+}
